7128502a18fe7650aae662872883315683b9412e
RachelCohen1 <33221815+RachelCohen1@users.noreply.github.com>
2018-06-26 13:32:45 +0300

    fix textarea validation error class

--- src/angular/form-elements/text-elements/textarea/textarea.component.html.ts
+++ src/angular/form-elements/text-elements/textarea/textarea.component.html.ts
@@ -3,7 +3,7 @@ export const template = `
     <label class="sdc-textarea__label" *ngIf="label" [ngClass]="{'required':required}">{{label}}</label>
     <textarea
         class="sdc-textarea__textarea {{classNames}}"
-        [ngClass]="{'error': (!valid && dirty), 'disabled':disabled}"
+        [ngClass]="{'error': (valid === false && dirty), 'disabled':disabled}"
         [attr.name]="name ? name : null"
         [placeholder]="placeHolder"
         [(ngModel)]="value"
