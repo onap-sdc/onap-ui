70f097a858c7deb5849e28205d43de8c0c0fa0da
onap-sdc <onap.sdc@gmail.com>
2018-06-27 14:26:49 +0300

    Update loader.component.html.ts

--- src/angular/loader/loader.component.html.ts
+++ src/angular/loader/loader.component.html.ts
@@ -1,11 +1,11 @@
 export const template = `
-<div class = "sdc-loader-wrapper" *ngIf="!global">
+<div class = "sdc-loader-wrapper" *ngIf="!global" data-tests-id="loader">
     <div class="sdc-loader-background" *ngIf="active">
         <div class="sdc-loader {{ size }}" *ngIf="active"></div>
     </div>
     <ng-content></ng-content>
 </div>
-<div  *ngIf="global&&active">
+<div  *ngIf="global&&active" data-tests-id="loader">
     <div class="sdc-loader-global-wrapper sdc-loader-background" >
         <div class="sdc-loader {{ size }}"></div>
     </div>
