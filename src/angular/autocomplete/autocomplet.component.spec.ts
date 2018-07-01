7db30b79f65cd00e6dc6c47f7d6d87c12df5d1bf
fridman <fridmaster@gmail.com>
2018-07-01 12:52:42 +0300

    fix for test of autocomplite

--- src/angular/autocomplete/autocomplet.component.spec.ts
+++ src/angular/autocomplete/autocomplet.component.spec.ts
@@ -52,22 +52,20 @@ describe("AutoComplite Tests", () => {
         expect(listHtml != null && listItemsCount === 0).toBeTruthy();
     }));
 
-    fit('Auto Complite build not empty DOM List', async(() => {
+    it('Auto Complite build not empty DOM List', async(() => {
         component.data = testData;
         component.dataSchema = {key: 'id', value: 'color'};
         component.placeholder = 'search text';
         component.label = 'search by color:';
-        component.onSearchQueryChanged('re');
-        console.info(component.complexData)
-        let res = component.autocompletePipe.transform(component.complexData, component.searchQuery);
+        component.searchQuery = '';
         fixture.detectChanges();
-        console.info(res);
+        component.onSearchQueryChanged('re');
+        fixture.detectChanges();
         fixture.whenStable().then(() => {
             const autoCompliteHtml = fixture.nativeElement;
-            console.info(component.searchQuery);
             const listHtml = autoCompliteHtml.querySelector('ul');
             const listItemsCount = autoCompliteHtml.querySelectorAll('li').length;
-            expect(listHtml != null && listItemsCount === 3).toBeTruthy();
+            expect(listHtml != null && listItemsCount === 2).toBeTruthy();
         });      
     }));
 
