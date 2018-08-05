4f84f637c09c53045dd759d69169eeb6eb2a95bd
Israel Lavi <israel.lavi@gmail.com>
2018-08-05 11:42:03 +0300

    Adding button opener component

--- src/angular/buttons/buttons.module.ts
+++ src/angular/buttons/buttons.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from "@angular/core";
 import { ButtonComponent } from "./button.component";
+import { ButtonFileOpenerComponent } from "./button-file-opener.component";
 import { CommonModule } from "@angular/common";
 import { SvgIconModule } from './../svg-icon/svg-icon.module';
 
 @NgModule({
     declarations: [
-        ButtonComponent
+        ButtonComponent,
+        ButtonFileOpenerComponent
     ],
     imports: [
         CommonModule,
         SvgIconModule
     ],
     exports: [
-        ButtonComponent
-
+        ButtonComponent,
+        ButtonFileOpenerComponent
     ],
 })
 export class ButtonsModule {
