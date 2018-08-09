fe32acf3bdd7f8c64294fd435ca10810d3b9819f
Israel Lavi <israel.lavi@gmail.com>
2018-08-09 13:29:37 +0300

    Adding support for button-file-opener

--- src/angular/buttons/button-file-opener.component.html.ts
+++ src/angular/buttons/button-file-opener.component.html.ts
@@ -1,17 +1,14 @@
 export const template = `
-<div (click)="fileOpener.click()"
-     class="sdc-button sdc-button__{{ type }} btn-{{ size }} {{ iconPositionClass }}"
+<div class="sdc-button sdc-button__{{ type }} sdc-button__file-opener btn-{{ size }} {{ iconPositionClass }}"
      [attr.data-tests-id]="testId"
      >
     {{ text }}
     <input
-        #fileOpener
         type="file"
         [attr.data-tests-id]="'file' + testId"
         [accept]="allowedExtensions"
         [disabled] = "disabled"
         (change)="onFileSelect($event)"
-        base-sixty-four-input
     />
 </div>
 `;
